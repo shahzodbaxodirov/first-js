d49a0d4473e73068ba46cf0808e43e3dcc2d70c4
shahzod <baxodirovshahzod772@gmail.com>
2022-11-03 09:28:30 +0500

    first js

---
+++ index.js
@@ -0,0 +1,17 @@
+var FIO = prompt("Familiyangiz");
+var address = prompt("Yashash joyingiz");
+var kasb = prompt("Kasbingiz");
+var oila = prompt("Oilangiz haqida");
+var car = prompt("Mashinangiz bormi");
+
+
+var hobbi = prompt("Bo`sh vatingizda niam bilan shug`ullanasiz");
+
+
+
+console.log(FIO);
+console.log(address);
+console.log(kasb);
+console.log(oila);
+console.log(car);
+alert(hobbi);
